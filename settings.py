459dc9f47cfb87b70cd1d4b528d7229388660a01
Semyon Aparin <sem0156@mail.ru>
2022-05-24 11:32:54 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+PORT = int(os.environ.get("PORT"))
+
+DEBUG = os.environ.get("DEBUG") == "True"
